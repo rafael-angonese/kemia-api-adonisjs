ac8ba8b705c345e6e02898c47a4718ac2df18d7c
appkemia <appkemia@gmail.com>
2021-03-13 12:52:50 -0300

    ssl true database

--- config/database.js
+++ config/database.js
@@ -78,6 +78,7 @@ module.exports = {
       user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', ''),
       database: Env.get('DB_DATABASE', 'adonis'),
+      ssl: true,
     },
     debug: Env.get('DB_DEBUG', false)
   }
