8c00e7258b5e2b9bacde9834cc365f2fd77ebf52
appkemia <appkemia@gmail.com>
2021-03-12 21:04:31 -0300

    ssl teste

--- server.js
+++ server.js
@@ -19,7 +19,20 @@
 
 const { Ignitor } = require('@adonisjs/ignitor')
 
+const path = require('path')
+const https = require('https')
+const fs = require('fs')
+
+// Certificate
+const options = {
+  key: fs.readFileSync(path.join(__dirname, './server.key')),
+  cert: fs.readFileSync(path.join(__dirname, './server.crt'))
+}
+
 new Ignitor(require('@adonisjs/fold'))
   .appRoot(__dirname)
-  .fireHttpServer()
+  .fireHttpServer((handler) => {
+    return https.createServer(options, handler)
+  })
+  // .fireHttpServer()
   .catch(console.error)
