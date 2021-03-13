9db4fecd11da0ac74884c5f8aa50270ed5c1b32e
appkemia <appkemia@gmail.com>
2021-03-12 21:27:58 -0300

    sdfafd

--- server.js
+++ server.js
@@ -19,20 +19,7 @@
 
 const { Ignitor } = require('@adonisjs/ignitor')
 
-const path = require('path')
-const https = require('https')
-const fs = require('fs')
-
-// Certificate
-const options = {
-  key: fs.readFileSync(path.join(__dirname, './server.key')),
-  cert: fs.readFileSync(path.join(__dirname, './server.crt'))
-}
-
 new Ignitor(require('@adonisjs/fold'))
   .appRoot(__dirname)
-  .fireHttpServer((handler) => {
-    return https.createServer(options, handler)
-  })
-  // .fireHttpServer()
+  .fireHttpServer()
   .catch(console.error)
